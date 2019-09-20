6524c0588d84be6b9aa90f70cdda1916a055faa0
arsummers <aliyasummers1@gmail.com>
2019-09-20 13:40:42 -0700

    models not found

--- book_list/bookapi/views.py
+++ book_list/bookapi/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from models import Books
+from rest_framework import viewsets
+from serializers import BooksSerializer
 
-# Create your views here.
+class BooksViewSet(viewsets.ModelViewSet):
+    serializer_class = BooksSerializer
