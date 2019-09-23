ac6b0bd5c4ee81c758a41323f3248aad297d79f5
arsummers <aliyasummers1@gmail.com>
2019-09-23 13:38:31 -0700

    basic django app

--- books/models.py
+++ books/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f'Name: {self.name}'
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Title: {self.title} \n Author: {self.author}'
