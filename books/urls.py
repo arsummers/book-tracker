21cad89a0db335659c772cea1cafb4028aaf84e5
arsummers <aliyasummers1@gmail.com>
2019-09-25 12:25:55 -0700

    urls wired

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from books import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.book_list),
+    path('books/<int:pk>/', views.book_detail),
 ]
