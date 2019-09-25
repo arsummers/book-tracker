2ac0c623beffff05716a9be84a359ed3bf529f38
arsummers <aliyasummers1@gmail.com>
2019-09-24 17:58:36 -0700

    first view added

--- books/views.py
+++ books/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
 from books.models import Author, Book
 from books.serializers import AuthorSerializer, BookSerializer
 
 def index(request):
     return HttpResponse('this a homepage?')
+
+@csrf_exempt
+def book_list(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = BookSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
 
