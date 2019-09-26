769477068c351d09ebb4bb69bd9b61f970ef439d
arsummers <aliyasummers1@gmail.com>
2019-09-25 20:46:13 -0700

    error - fix tomorrow

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from books import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('books/', views.book_list),
     path('books/<int:pk>/', views.book_detail),
 ]
--- books/views.py
+++ books/views.py
@@ -6,9 +6,6 @@ from rest_framework.parsers import JSONParser
 from books.models import Author, Book
 from books.serializers import AuthorSerializer, BookSerializer
 
-def index(request):
-    return HttpResponse('this a homepage?')
-
 @csrf_exempt
 def book_list(request):
     if request.method == 'GET':
