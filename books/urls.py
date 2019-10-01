727f7fe5df72005975ee6805e8e1993d706a3d80
arsummers <aliyasummers1@gmail.com>
2019-10-01 12:40:27 -0700

    author views finished

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,6 @@ from books import views
 urlpatterns = [
     path('books/', views.book_list),
     path('books/<int:pk>/', views.book_detail),
+    path('authors/', views.author_list),
+    path('authors/<int:pk>/', views.author_detail),
 ]
--- books/views.py
+++ books/views.py
@@ -63,5 +63,27 @@ def author_list(request, format=None):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+@api_view(['GET', 'PUT', 'DELETE'])
+def author_detail(request, pk, format=None):
+    """
+    work with individual author objects
+    """
+    try:
+        author = Author.objects.get(pk=pk)
+    except Author.DoesNotExist():
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = AuthorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
